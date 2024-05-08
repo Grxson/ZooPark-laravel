8e10c025bb3cd62f396874591a9295698a51f1c8
Grxson <gael.grxson@gmail.com>
2024-05-08 00:59:25 -0600

    initial commit
    de regreso

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
 });
 
 Route::view('/zoo/home', '/zoo/index');
-Route::view('/zoo/instalaciones', '/zoo/intalaciones');
+Route::view('/zoo/instalaciones', '/zoo/instalaciones');
 Route::view('/zoo/form', '/zoo/form');
 Route::view('/zoo/registrar_visita', '/zoo/registrar_visita');
 Route::view('/zoo/servicios', '/zoo/servicios');
